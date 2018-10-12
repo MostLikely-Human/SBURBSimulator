45a5f881e18cf281e311617777ceb1e0b87b5c05
MostLikely-Human <39070379+MostLikely-Human@users.noreply.github.com>
2018-10-12 10:11:57 -0700

    TImestone handles

--- web/scripts/GameEntities/ClasspectStuff/Aspects/Timestone.dart
+++ web/scripts/GameEntities/ClasspectStuff/Aspects/Timestone.dart
@@ -19,6 +19,9 @@ class Timestone extends Aspect {
     ..shirt_dark = '#8f8f8f'
     ..pants_light = '#a88e00'
     ..pants_dark = '#756300';
+  
+  @override
+  List<String> handles = new List<String>.unmodifiable(<String>["Stone", "Sauron", "Stupid", "Selfish", "Sock", "Stuck", "Selian", "Sandals", "Sans"]);
 
   Timestone(int id) :super(id, "Timestone", isCanon: false, isInternal: true, isMLHFanon: true); //It's Proxxima's and Egg's, but I'm coding it.
 }
