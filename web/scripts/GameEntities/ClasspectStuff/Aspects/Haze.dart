75d543d0c00b66c10ceeb2adf1c5be29b0ae152f
MostLikely-Human <39070379+MostLikely-Human@users.noreply.github.com>
2018-11-22 16:53:46 -0800

    Finally finished Haze colors

--- web/scripts/GameEntities/ClasspectStuff/Aspects/Haze.dart
+++ web/scripts/GameEntities/ClasspectStuff/Aspects/Haze.dart
@@ -13,13 +13,13 @@ class Haze extends Aspect {
     ..aspect_dark = '#cc6200'
     ..shoe_light = '#cc6200'
     ..shoe_dark = '#994900'
-    ..cloak_light = '#8a9499' //
-    ..cloak_mid = '#5c6366' //
-    ..cloak_dark = '#2e3133' //
-    ..shirt_light = '#86acbf' //
-    ..shirt_dark = '#627e8c' //
-    ..pants_light = '#8fa7b3' //
-    ..pants_dark = '#667780'; //
+    ..cloak_light = '#cc9766'
+    ..cloak_mid = '#99714d'
+    ..cloak_dark = '#664b33' 
+    ..shirt_light = '#e69245'
+    ..shirt_dark = '#b37136'
+    ..pants_light = '#cc823d'
+    ..pants_dark = '#99622e';
 
   Haze(int id) :super(id, "Haze", isCanon: false, isMLHFanon: true);
 
